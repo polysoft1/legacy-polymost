f14e20fcf8ffb599bcb3b034eb2133d884a67571
Keith Va <keithbunnies@gmail.com>
2017-10-29 14:34:57 -0400

    Protobuffer support!

--- src/headers/PolyMostCommand.h
+++ src/headers/PolyMostCommand.h
@@ -5,14 +5,20 @@
 #include <string>
 #include <vector>
 #include "PolyMost.h"
+#include "polymost.pb.h"
 
 class PolyMostCommand : public ICommand {
 public:
 	PolyMostCommand(PolyMost& main);
+	~PolyMostCommand();
 	bool onCommand(std::vector<std::string>);
 	std::vector<std::string>* onTabCompletion(std::vector<std::string>, int);
 private:
 	PolyMost& main;
+	
+	polymost::user user;
+	polymost::server server;
+
 	bool loginCommand(std::vector<std::string>);
 	bool listTeamsCommand(std::vector<std::string>);
 	bool listChannelsCommand(std::vector<std::string>);
