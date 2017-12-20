0d299c47f17eccb26df2cb8fb9a01ac5961a72a1
lizardfreak321 <lizardfreak7@gmail.com>
2017-12-20 10:34:00 -0500

    Updated to work with newest version of PolyChatCore

--- src/headers/PolyMost.h
+++ src/headers/PolyMost.h
@@ -4,8 +4,10 @@
 #include "include/IProtocolPlugin.h"
 #include "include/IPlugin.h"
 #include "include/Core.h"
+#include "include/Registrant.h"
 #include <string>
 #include <vector>
+
 class PolyMost : public IProtocolPlugin {
 public:
 	PolyMost();
