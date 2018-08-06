e69b1e55164d48966f04fed69ac3e2fd021ac844
lizardfreak321 <lizardfreak7@gmail.com>
2018-08-06 16:54:46 -0400

    Updated MattermostAccount to contain useful info.

--- src/headers/MattermostAccount.h
+++ src/headers/MattermostAccount.h
@@ -4,14 +4,40 @@
 #include <string>
 #include "include/IAccount.h"
 
+/**
+ * Represents a user on Mattermost. Can be the logged in user
+ * or just another team member.
+ */
 class MattermostAccount : public IAccount {
+private:
+	std::string uid, email, username, first_name, last_name, nickname, locale;
+
 public:
-	MattermostAccount() {
+	MattermostAccount(std::string uid, std::string email, std::string username, std::string first_name,
+		std::string last_name, std::string nickname, std::string locale) :
+		uid(uid), email(email), username(username), first_name(first_name),
+		nickname(nickname), locale(locale)
+	{
 
 	}
 
+	std::string getUID() {
+		return username;
+	}
+	std::string getEmail() {
+		return username;
+	}
 	std::string getUsername() {
-		return "NOT IMPLEMENTED";
+		return username;
+	}
+	std::string getFirstName() {
+		return username;
+	}
+	std::string getNickName() {
+		return username;
+	}
+	std::string getLocale() {
+		return username;
 	}
 };
 
