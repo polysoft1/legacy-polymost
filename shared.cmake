ecea955ac31aaf92848ad9a9678cdd2f27e48225
Keith Valin <keith@mailtux.net>
2019-11-23 14:25:11 -0500

    Functioning json creation

--- shared.cmake
+++ shared.cmake
@@ -2,7 +2,7 @@ project(PolyMost)
 
 include_directories(${POLYCHAT})
 set(executable_filename ${CMAKE_SHARED_LIBRARY_PREFIX}PolyMost${CMAKE_SHARED_LIBRARY_SUFFIX})
-set(xml_file ${OUTPUT_DIR}/plugin.xml)
+set(json_file ${OUTPUT_DIR}/plugin.json)
 
 ADD_LIBRARY(PolyMost SHARED ${src})
 
@@ -17,15 +17,15 @@ list(APPEND DYN_INCLUDE_DIRS ${PROJECT_BINARY_DIR})
 #endif()
 set_target_properties(PolyMost PROPERTIES INCLUDE_DIRECTORIES "${DYN_INCLUDE_DIRS}")
 
-add_custom_target(xml_creation)
-add_dependencies(xml_creation PolyMost)
-add_custom_command(TARGET xml_creation
+add_custom_target(json_creation)
+add_dependencies(json_creation PolyMost)
+add_custom_command(TARGET json_creation
 	POST_BUILD
-	COMMAND cmake -DHOME="${CMAKE_HOME_DIRECTORY}" -DEXECUTABLE="${executable_filename}" -DOUT_DIR="${OUTPUT_DIR}" -DXML_FILE="plugin.xml" -DCMAKE_SYSTEM_NAME="${CMAKE_SYSTEM_NAME}" -P ${CMAKE_HOME_DIRECTORY}/xmlcreate.cmake
+	COMMAND cmake -DHOME="${CMAKE_HOME_DIRECTORY}" -DEXECUTABLE="${executable_filename}" -DOUT_DIR="${OUTPUT_DIR}" -DJSON_FILE="plugin.json" -DCMAKE_SYSTEM_NAME="${CMAKE_SYSTEM_NAME}" -P ${CMAKE_HOME_DIRECTORY}/jsoncreate.cmake
 ) 
 
 add_custom_target(create_zip
-	ALL COMMAND ${CMAKE_COMMAND} -E tar "cfv" "PolyMost.zip" --format=zip "${executable_filename}" "${xml_file}"
+	ALL COMMAND ${CMAKE_COMMAND} -E tar "cfv" "PolyMost.zip" --format=zip "${executable_filename}" "${json_file}"
 	WORKING_DIRECTORY ${OUTPUT_DIR}
 )
-add_dependencies(create_zip xml_creation PolyMost)
+add_dependencies(create_zip json_creation PolyMost)
